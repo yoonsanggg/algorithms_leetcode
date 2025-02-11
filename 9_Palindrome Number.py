1a9ab25928e7725c428594d7c684777f2e8c6ba6
yoonsanggg <yoonsangtommy@gmail.com>
2025-02-11 09:38:13 +0900

    day2

--- 9_Palindrome Number.py
+++ 9_Palindrome Number.py
@@ -1,14 +1,51 @@
-# x= 1112
-x= 121
+x=121
+x = [i for i in str(x)]
+xx=[i for i in x]
+pop_backword=[]
+frontword=[]
 
-def ax(x):
-    x=str(x)
-    y=[i for i in x]
-    z=[i for i in x[::-1]]
-    if y==z:
-        print('true')
-    else:
-        print('false')
+for i in xx:
+    pop_backword.append(x.pop())
+    frontword.append(i)
+    
+if pop_backword == frontword:
+    print(True)
+else:
+    print(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x= 1112
+# x= 121
+
+# def ax(x):
+#     x=str(x)
+#     y=[i for i in x]
+#     z=[i for i in x[::-1]]
+#     if y==z:
+#         print('true')
+#     else:
+#         print('false')
     
         
-ax(x)
+# ax(x)
